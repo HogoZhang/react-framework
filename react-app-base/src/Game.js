cbdaacd57bba2c075bbdd7304c6231d6df4ce0db
HogoZhang <350010981@qq.com>
2020-08-18 23:41:39 +0800

    test base-app

---
+++ react-app-base/src/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        你好,React!
+      </div>
+    )
+  }
+}
+
+export default Game;
